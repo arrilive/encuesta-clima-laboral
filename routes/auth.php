0e6b348af2a20c33320090e95f6f40291da61b1e
arrilive <lv20051309@gmail.com>
2026-07-07 14:57:37 -0600

    fix(auth): eliminar flujo de olvido de contraseña de Breeze

--- routes/auth.php
+++ routes/auth.php
@@ -7,12 +7,6 @@ use Livewire\Volt\Volt;
 Route::middleware('guest')->group(function () {
     Volt::route('login', 'pages.auth.login')
         ->name('login');
-
-    Volt::route('forgot-password', 'pages.auth.forgot-password')
-        ->name('password.request');
-
-    Volt::route('reset-password/{token}', 'pages.auth.reset-password')
-        ->name('password.reset');
 });
 
 Route::middleware('auth')->group(function () {
@@ -22,7 +16,4 @@ Route::middleware('auth')->group(function () {
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
         ->middleware(['signed', 'throttle:6,1'])
         ->name('verification.verify');
-
-    Volt::route('confirm-password', 'pages.auth.confirm-password')
-        ->name('password.confirm');
 });
